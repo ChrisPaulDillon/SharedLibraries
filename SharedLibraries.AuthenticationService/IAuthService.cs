0d08d7a7065aaa7b86c51f9b3284cf95844c6d2b
Chris Dillon <chris.dillon@serialaffinity.co.uk>
2021-03-19 13:27:32 +0000

    Initial commit

---
+++ SharedLibraries.AuthenticationService/IAuthService.cs
@@ -0,0 +1,7 @@
+﻿namespace PowerBuddy.AuthenticationService
+{
+    public interface IAuthService
+    {
+        public string GenerateJwtToken(string userId, string userName, bool usingMetric, bool firstVisit, int memberStatusId);
+    }
+}
