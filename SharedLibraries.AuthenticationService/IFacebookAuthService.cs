7208a188da32f6d54b1961e9a344e57d8612fcf4
Chris Dillon <chris.dillon@serialaffinity.co.uk>
2021-03-19 14:22:36 +0000

    Added proper version path

--- SharedLibraries.AuthenticationService/IFacebookAuthService.cs
+++ SharedLibraries.AuthenticationService/IFacebookAuthService.cs
@@ -5,6 +5,7 @@ namespace SharedLibraries.AuthenticationService
 {
     public interface IFacebookAuthService
     {
+        //test
 	    Task<FacebookTokenValidationResult> ValidateAccessTokenAsync(string accessToken);
         Task<FacebookUserInfoResult> GetUserInfoAsync(string accessToken);
     }
